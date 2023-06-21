99ae61881c15d7e1ca8c4a141e5cdf4f37cf5477
Ashish <ashishd52280@gmail.com>
2023-06-21 09:46:02 +0530

    All modules are completed. code is working fine!

--- go-bookstore/pkg/config/app.go
+++ go-bookstore/pkg/config/app.go
@@ -14,7 +14,9 @@ var (
 
 func Connect() {
 	// This is for opening connection to mysql. ashish is the username, ashishd@57@ is the password and simplerest is the table name
-	d, err := gorm.Open("mysql", "ashish:ashishd@57@/simplerest?charset=utf8&parseTime=True&loc=Local")
+	// d, err := gorm.Open("mysql", "root:Ashu2124#/simplerest?charset=utf8&parseTime=True&loc=Local")
+	dsn := "root:Ashu2124#@tcp(127.0.0.1:3306)/simplerest?charset=utf8mb4&parseTime=True&loc=Local"
+	d, err := gorm.Open("mysql", dsn)
 
 	if err != nil {
 		panic(err)
